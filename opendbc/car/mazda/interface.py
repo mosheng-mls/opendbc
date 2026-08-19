9e6d17cbbcd2777587abb255b9bbb929319a6832
mosheng-mls <mosheng-mls@users.noreply.github.com>
2026-08-19 19:34:21 +0800

    feat(mazda): add guarded BM radar replay interface

--- opendbc/car/mazda/interface.py
+++ opendbc/car/mazda/interface.py
@@ -4,12 +4,14 @@ from opendbc.car.common.conversions import Conversions as CV
 from opendbc.car.interfaces import CarInterfaceBase
 from opendbc.car.mazda.carcontroller import CarController
 from opendbc.car.mazda.carstate import CarState
+from opendbc.car.mazda.radar_interface import RadarInterface
 from opendbc.car.mazda.values import CAR, LKAS_LIMITS, LOW_DEMAND_P_TORQUE_CAP
 
 
 class CarInterface(CarInterfaceBase):
   CarState = CarState
   CarController = CarController
+  RadarInterface = RadarInterface
 
   @staticmethod
   def _get_params(ret: structs.CarParams, candidate, fingerprint, car_fw, alpha_long, is_release, docs) -> structs.CarParams:
